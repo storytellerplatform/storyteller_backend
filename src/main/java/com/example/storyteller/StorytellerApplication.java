5b2e3e7fc487cb1702fa11504c76e6b599357746
onandon <047633597q@gmail.com>
2023-09-14 12:28:28 +0800

    initial commit

---
+++ src/main/java/com/example/storyteller/StorytellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.storyteller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorytellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorytellerApplication.class, args);
+	}
+
+}
