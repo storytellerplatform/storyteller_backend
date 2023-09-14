246068738894eda27fcbf7014fc7046e7b9941fc
onandon <047633597q@gmail.com>
2023-09-14 12:42:01 +0800

    test

--- src/main/java/com/example/storyteller/StorytellerApplication.java
+++ src/main/java/com/example/storyteller/StorytellerApplication.java
@@ -2,7 +2,10 @@ package com.example.storyteller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class StorytellerApplication {
 
@@ -10,4 +13,8 @@ public class StorytellerApplication {
 		SpringApplication.run(StorytellerApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello() {
+		return "Hello world!";
+	}
 }
