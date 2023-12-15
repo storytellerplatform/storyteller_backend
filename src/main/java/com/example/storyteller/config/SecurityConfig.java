09e3c6c8b0758a4aedbe3474fe4b734fa01cc9c2
onandon <047633597q@gmail.com>
2023-12-15 16:09:44 +0800

    更改至 nordvpn

--- src/main/java/com/example/storyteller/config/SecurityConfig.java
+++ src/main/java/com/example/storyteller/config/SecurityConfig.java
@@ -104,7 +104,8 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://100.87.143.63:3000"));
+//        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://100.87.143.63:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
         configuration.setAllowCredentials(true);
